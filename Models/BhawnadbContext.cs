db27e5075fa133efa862a5251b5c2314dc16f252
Bhawna Gunwani <bhawna.gunwani@gmail.com>
2025-02-13 16:25:54 +0530

    Latest Commit 2

---
+++ Models/BhawnadbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAzureDemoApplication.Models;
+
+public partial class BhawnadbContext : DbContext
+{
+    public BhawnadbContext()
+    {
+    }
+
+    public BhawnadbContext(DbContextOptions<BhawnadbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.DepartmentId).HasName("PK__Departme__B2079BEDE3B014E4");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Employee__7AD04F11ECA0984D");
+
+            entity.HasIndex(e => e.Email, "UQ__Employee__A9D10534A1FD44F5").IsUnique();
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Salary).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .HasConstraintName("FK__Employees__Depar__5FB337D6");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
